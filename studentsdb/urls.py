1aa5e1ae854ce9a75370a8f3d4ea9fe03c682259
Andriy Zelenyuk <azelenyuk@gmail.com>
2014-12-16 14:45:27 +0200

    add %url%

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -5,6 +5,33 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'studentsdb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # Students urls
+	url(r'^$', 'students.views.students_list', name='home'),
+	
+	url(r'^students/add/$', 'students.views.students_add',
+	name="{% url "students_add" %}"),
+	
+	url(r'^students/(?P<sid>\d+)/edit/$',
+	'students.views.students_edit',
+	name="{% url "students_edit" %}"),
+	
+	url(r'^students/(?P<sid>\d+)/delete/$',
+	'students.views.students_delete',
+	name="{% url "students_delete" %}"),
+
+	# Groups urls
+	url(r'^groups/$', 'students.views.groups_list', name='groups'),
+	
+	url(r'^groups/add/$', 'students.views.groups_add',
+	name='groups_add'),
+	
+	url(r'^groups/(?P<gid>\d+)/edit/$',
+	'students.views.groups_edit',
+	name='groups_edit'),
+	
+	url(r'^groups/(?P<gid>\d+)/delete/$',
+	'students.views.groups_delete',
+	name='groups_delete'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
