4ec277e90633962f562f9682939b7431a4c39454
Andriy Zelenyuk <azelenyuk@gmail.com>
2014-12-24 15:18:31 +0200

    migration to mysql

--- students/views/students.py
+++ students/views/students.py
@@ -2,23 +2,34 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from ..models import Student
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Віталій',
-         'last_name': u'Подоба',
-         'ticket': 235,
-         'image': 'img/me.jpeg'},
-        {'id': 2,
-         'first_name': u'Корост',
-         'last_name': u'Андрій',
-         'ticket': 2123,
-         'image': 'img/piv.png'},
-    )
+    students = Student.objects.all()
+
+    # try to order students list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+    
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        students = paginator.page(paginator.num_pages)
+        
     return render(request, 'students/students_list.html',
-        {'students': students})
+    {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
